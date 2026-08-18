8771efa99aefc3b73e9d5a1f4d4a5d760d9126d5
adamyudhistiramuhtar-byte <adamyudhistiramuhtar@gmail.com>
2026-08-18 19:40:10 +0700

    chore(bootstrap): inisialisasi berkas basis SemesterResource.php
    Basis kode eksisting untuk apps/api/app/Http/Resources/SemesterResource.php.
    
    Refs: BOOTSTRAP

---
+++ apps/api/app/Http/Resources/SemesterResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SemesterResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'name' => $this->name,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'is_active' => $this->is_active,
+        ];
+    }
+}
